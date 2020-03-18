ad7ff0e0be6973510b8902f1877bd102565993bc
AbyssLink <26038696+AbyssLink@users.noreply.github.com>
2020-03-18 23:43:57 +0800

    optimize chart animation

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,64 +1,59 @@
-import React, { Component } from "react";
+import React from "react";
 import { Title } from "react-admin";
-import Analytics from "./apexChart";
+import { Analytics } from "./apexChart";
 import Grid from "@material-ui/core/Grid";
-import jsonServerProvider from "ra-data-json-server";
+import { useQueryWithStore, Loading, Error } from "react-admin";
 
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
-
-class DashHome extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {
-        userIdList: [],
-        idList: []
-      }
-    };
-    this.getList();
+export const DashHome = props => {
+  const { loaded, error, data } = useQueryWithStore({
+    type: "getList",
+    resource: "posts",
+    payload: {
+      pagination: { page: 1, perPage: 8 },
+      sort: { field: "title", order: "ASC" },
+      filter: { author_id: 12 }
+    }
+  });
+  if (!loaded) {
+    return <Loading />;
   }
-  getList = () => {
-    dataProvider
-      .getList("posts", {
-        pagination: { page: 1, perPage: 8 },
-        sort: { field: "title", order: "ASC" },
-        filter: { author_id: 12 }
-      })
-      .then(response => {
-        const posts = response.data;
-        for (const post of posts) {
-          this.state.data.userIdList.push(post["userId"]);
-          this.state.data.idList.push(post["id"]);
-        }
-      });
-  };
-  render() {
-    return (
-      <div>
-        <Title title="Welcome to the administration" />
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Analytics type="bar" data={this.state.data}></Analytics>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Analytics type="scatter" data={this.state.data}></Analytics>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Analytics type="line" data={this.state.data}></Analytics>
-          </Grid>
-          <Grid item xs={6} sm={4}>
-            <Analytics type="area" data={this.state.data}></Analytics>
-          </Grid>
-          <Grid item xs={6} sm={4}>
-            <Analytics type="heatmap" data={this.state.data}></Analytics>
-          </Grid>
-          <Grid item xs={6} sm={4}>
-            <Analytics type="bar" data={this.state.data}></Analytics>
-          </Grid>
+  if (error) {
+    return <Error />;
+  }
+  const { xs, ys } = formatData(data);
+  return (
+    <div>
+      <Title title="Welcome to the administration" />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Analytics type="bar" data={{ xs, ys }}></Analytics>
         </Grid>
-      </div>
-    );
-  }
-}
+        <Grid item xs={12} sm={6}>
+          <Analytics type="area" data={{ xs, ys }}></Analytics>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Analytics type="line" data={{ xs, ys }}></Analytics>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Analytics type="area" data={{ xs, ys }}></Analytics>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Analytics type="heatmap" data={{ xs, ys }}></Analytics>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Analytics type="bar" data={{ xs, ys }}></Analytics>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default DashHome;
+const formatData = posts => {
+  let xs = [];
+  let ys = [];
+  for (const post of posts) {
+    xs.push(post["userId"]);
+    ys.push(post["id"]);
+  }
+  return { xs, ys };
+};
--- src/apexChart.js
+++ src/apexChart.js
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
 import Chart from "react-apexcharts";
-import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import CardContent from "@material-ui/core/CardContent";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import Card from "@material-ui/core/Card";
 
-class Analytics extends Component {
+export class Analytics extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loaderStyle: "block",
-      contentStyle: "none",
       options: {
         chart: {
           id: "apexchart-example"
         },
         xaxis: {
-          categories: props.data.idList
+          categories: this.props.data.xs
         },
         theme: {
           mode: "light",
@@ -27,51 +23,27 @@ class Analytics extends Component {
       series: [
         {
           name: "series-1",
-          data: props.data.userIdList
+          data: this.props.data.ys
         }
-      ],
-      useStyles: makeStyles(theme => ({
-        root: {
-          flexGrow: 1
-        },
-        paper: {
-          padding: theme.spacing(2),
-          textAlign: "center",
-          color: theme.palette.text.secondary
-        }
-      }))
+      ]
     };
-  }
-  componentDidMount() {
-    console.log(this.state.series[0].data);
-    this.setState({ loaderStyle: "none", contentStyle: "block" });
   }
   render() {
     return (
-      <div>
-        <Card>
-          <CircularProgress
-            color="secondary"
-            style={{ display: this.state.loaderStyle }}
+      <Card>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            {this.state.options.chart.id}
+          </Typography>
+          <Chart
+            options={this.state.options}
+            series={this.state.series}
+            type={this.props.type}
+            width={"100%"}
+            height={"220"}
           />
-          <div style={{ display: this.state.contentStyle }}>
-            <CardContent>
-              <Typography color="textSecondary" gutterBottom>
-                {this.state.options.chart.id}
-              </Typography>
-              <Chart
-                options={this.state.options}
-                series={this.state.series}
-                type={this.props.type}
-                width={"100%"}
-                height={"200"}
-              />
-            </CardContent>
-          </div>
-        </Card>
-      </div>
+        </CardContent>
+      </Card>
     );
   }
 }
-
-export default Analytics;
