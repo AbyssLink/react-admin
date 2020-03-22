3b4cd125d965e7237d5c0fe74fb9001e5ae44490
AbyssLink <26038696+AbyssLink@users.noreply.github.com>
2020-03-23 03:18:18 +0800

    add stock chart support

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Admin, Resource } from "react-admin";
 import { DashHome } from "./Dashboard";
 import { UserList } from "./users";
+import { StockUSChart } from "./stockChart";
 import authProvider from "./authProvider";
 import jsonServerProvider from "ra-data-json-server";
 
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+// const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const dataProvider = jsonServerProvider("http://127.0.0.1:5000");
 const App = () => (
   <Admin
     // dashboard={Dashboard}
     authProvider={authProvider}
     dataProvider={dataProvider}
   >
-    <Resource name="posts" list={DashHome} />
-    <Resource name="users" list={UserList} />
+    {/* <Resource name="posts" list={DashHome} /> */}
+    {/* <Resource name="users" list={UserList} /> */}
+    <Resource name="stocks" list={StockUSChart} />
   </Admin>
 );
 
