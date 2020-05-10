8cb4382bb0639e6552d8f951825873025ba40888
AbyssLink <abysslink@outlook.com>
2020-05-10 13:30:57 +0800

    add request parameter(dirty method)

--- src/distribution/distribBoard.js
+++ src/distribution/distribBoard.js
@@ -64,7 +64,9 @@ const useStyles = makeStyles((theme) => ({
 export const DistribBoard = (props) => {
   const classes = useStyles();
   const [symbolId, setSymbolId] = useState("AAPL");
-  const [searchId, setSearchId] = useState("AAPL");
+  const [ratio, setRatio] = useState(0.1);
+  const [days, setDays] = useState(220);
+  const [searchId, setSearchId] = useState("AAPL|0.1|220");
   const [time] = useState(60);
   const [height, setHeight] = React.useState(315);
   const [gridSize, setGridSize] = React.useState(6);
@@ -104,7 +106,7 @@ export const DistribBoard = (props) => {
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
-      setSearchId(symbolId);
+      setSearchId(symbolId + "|" + ratio + "|" + days);
       console.log("enter press here! ");
     }
   };
@@ -125,8 +127,22 @@ export const DistribBoard = (props) => {
             </IconButton>
             <InputBase
               className={classes.input}
-              placeholder="Search symbols from AKShare"
+              placeholder="Symbol"
               onChange={(event) => setSymbolId(event.target.value)}
+              onKeyPress={handleKeyPress}
+            />
+            <Divider className={classes.divider} orientation="vertical" />
+            <InputBase
+              className={classes.input}
+              placeholder="Ratio"
+              onChange={(event) => setRatio(event.target.value)}
+              onKeyPress={handleKeyPress}
+            />
+            <Divider className={classes.divider} orientation="vertical" />
+            <InputBase
+              className={classes.input}
+              placeholder="Days"
+              onChange={(event) => setDays(event.target.value)}
               onKeyPress={handleKeyPress}
             />
             <IconButton
@@ -180,39 +196,6 @@ export const DistribBoard = (props) => {
             >
               <ZoomOutMapIcon />
             </IconButton>
-            <Divider className={classes.divider} orientation="vertical" />
-            <IconButton
-              // color="primary"
-              className={classes.iconButton}
-              aria-label="chart_type"
-              aria-describedby={id}
-              variant="contained"
-              onClick={handleGridChangerClick}
-            >
-              <ViewComfyIcon />
-            </IconButton>
-            <Divider className={classes.divider} orientation="vertical" />
-            <FormControl className={classes.formControl}>
-              <Select
-                labelId="demo-controlled-open-select-label"
-                id="demo-controlled-open-select"
-                value={searchId}
-                displayEmpty
-                onChange={handleSearchIdChange}
-              >
-                <MenuItem value={searchId}>CUSTOM</MenuItem>
-                <MenuItem value={"AAPL"}>AAPL</MenuItem>
-                <MenuItem value={"AMZN"}>AMZN</MenuItem>
-                <MenuItem value={"FB"}>FB</MenuItem>
-                <MenuItem value={"GOOG"}>GOOG</MenuItem>
-                <MenuItem value={"LK"}>LK</MenuItem>
-                <MenuItem value={"MSFT"}>MSFT</MenuItem>
-                <MenuItem value={"NFLX"}>NFLX</MenuItem>
-                <MenuItem value={"SNE"}>SNE</MenuItem>
-                <MenuItem value={"TLSA"}>TLSA</MenuItem>
-                <MenuItem value={"TSLA"}>TSLA</MenuItem>
-              </Select>
-            </FormControl>
           </Paper>
         </Grid>
         <Grid item xs={12}>
