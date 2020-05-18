af6dc6dad4dc6df77109e8ccdb2e895a1c1cfd50
AbyssLink <abysslink@outlook.com>
2020-05-18 14:59:47 +0800

    add introduction card in dashbord page

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,32 +1,59 @@
 import Box from "@material-ui/core/Box";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 import React from "react";
 import { Title } from "react-admin";
+import FeaturedPost from "./postCard";
+
+const featuredPosts = [
+  {
+    href: "https://github.com/abysslink/react-admin",
+    title: "Front-end",
+    date: "Last update: May 18, 2020",
+    description: "Based on React-admin, Material-UI.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTO0ELOQrF87Kw9otGVLQnjy25XZ6IuLjLMVPm3tG2eCK_2AmqX&usqp=CAU",
+    imageText: "Image Text",
+  },
+  {
+    href: "https://github.com/abysslink/stoks-app",
+    title: "Back-end",
+    date: "Last update: May 18, 2020",
+    description: "Based on Flask, Pandas, Sklearn, Scipy.",
+    image:
+      "https://i1.wp.com/www.corellis.eu/wp-content/uploads/2018/09/logo-flask.png",
+    imageText: "Image Text",
+  },
+  {
+    href:
+      "https://www.coursera.org/learn/python-statistics-financial-analysis/home/welcome",
+    title: "Course",
+    date: "Last update: May 18, 2020",
+    description: "Based on python statistics financial analysis by Xuhu Wan",
+    image:
+      "https://lh3.googleusercontent.com/13U5Vnio6xeZwOUzpOqf6lCq3phOUw80y4e9VLAONVfkEqYbME5ySDvg28CPien5dHs",
+    imageText: "Image Text",
+  },
+  {
+    href: "https://github.com/abysslink",
+    title: "About",
+    date: "Last commit: May 18, 2020",
+    description: "Abysslink. a computer science student.",
+    image: "https://avatars1.githubusercontent.com/u/26038696?s=60&v=4",
+    imageText: "Image Text",
+  },
+];
 
 export default () => (
   <div>
     <Title title="Homepage" />
     <Box m={3}>
-      <Container maxWidth="sm">
-        <Card>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              Welcome to the Finance Helper
-            </Typography>
-            <Typography component="p">
-              Front-end:{" "}
-              <a href="https://github.com/AbyssLink/react-admin">react-admin</a>
-            </Typography>
-            <Typography component="p">
-              Back-end:{" "}
-              <a href="https://github.com/AbyssLink/stoks-app">stoks-app</a>
-            </Typography>
-            <div>Based on: Material-UI, React-admin, Flask</div>
-          </CardContent>
-        </Card>
+      <Container maxWidth="lg">
+        <Grid container spacing={4}>
+          {featuredPosts.map((post) => (
+            <FeaturedPost key={post.title} post={post} />
+          ))}
+        </Grid>
       </Container>
     </Box>
   </div>
